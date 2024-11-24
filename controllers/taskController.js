6a92c2db06777a1ebf3fdc4696f73c9848dc23ba
pedro-crs <crs.pedroh@gmail.com>
2024-11-24 20:07:07 -0300

    Updates - Tasks functionalities
    Added category routes/model/controller to create/list tasks

---
+++ controllers/taskController.js
@@ -0,0 +1,40 @@
+const Category = require("../models/Category");
+const Task = require("../models/Task");
+
+exports.getTasks = async (req, res) => {
+	try {
+		const tasks = await Task.findAll({
+			attributes: { exclude: ["category_id"] },
+			include: {
+				model: Category,
+				attributes: ["title", "id", "color"]
+			}
+		});
+		res.status(200).json(tasks);
+	} catch (error) {
+		console.error("Erro ao buscar tarefas:", error);
+		res.status(500).json({ error: "Erro ao buscar tarefas:" });
+	}
+};
+
+exports.createTask = async (req, res) => {
+	const { userId, title, categoryId, isCompleted } = req.body;
+
+	try {
+		if (!userId || !title || !categoryId) {
+			return res.status(400).json({ message: "Campos obrigatórios estão faltando" });
+		}
+
+		const newTask = await Task.create({
+			user_id: userId,
+			title,
+			category_id: categoryId,
+			isCompleted: isCompleted
+		});
+
+		res.status(201).json({ message: "Tarefa criada com sucesso", task: newTask });
+	} catch (error) {
+		console.error("Erro ao criar a tarefa:", error.message);
+		res.status(500).json({ message: "Erro ao criar a tarefa", error: error.message });
+	}
+};
---
+++ models/Task.js
@@ -0,0 +1,38 @@
+const { DataTypes } = require("sequelize");
+const { sequelize } = require("../config/database");
+const Category = require("./Category");
+
+const Task = sequelize.define("Task", {
+	id: {
+		type: DataTypes.INTEGER,
+		primaryKey: true,
+		autoIncrement: true,
+	},
+	user_id: {
+		type: DataTypes.INTEGER,
+		allowNull: false,
+	},
+	title: {
+		type: DataTypes.STRING,
+		allowNull: false,
+	},
+	category_id: {
+		type: DataTypes.INTEGER,
+		allowNull: false,
+	},
+	created_at: {
+		type: DataTypes.DATE,
+		defaultValue: DataTypes.NOW,
+	},
+	isCompleted: {
+		type: DataTypes.BOOLEAN,
+		defaultValue: false,
+	},
+}, {
+	tableName: "tasks",
+	timestamps: false,
+});
+
+Task.belongsTo(Category, { foreignKey: "category_id" });
+
+module.exports = Task;
