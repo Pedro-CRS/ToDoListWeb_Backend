1f0ce1d6320461bcc87de986405132b1a6fb402c
pedro-crs <crs.pedroh@gmail.com>
2024-11-27 13:51:52 -0300

    Added token verification
    Added token verification middleware to auth routes and protected category and task routes with token verification

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,13 @@
 const express = require("express");
-const { register, login } = require("../controllers/authController");
+const { register, login, verifyToken } = require("../controllers/authController");
 
 const router = express.Router();
 
-router.post("/register", register);
-router.post("/login", login);
+router.post("/register", verifyToken, register);
+router.post("/login", verifyToken, login);
+
+router.get('/protected', verifyToken, (req, res) => {
+	res.json({ message: 'Você tem acesso a esta rota protegida!', user: req.user });
+});
 
 module.exports = router;
