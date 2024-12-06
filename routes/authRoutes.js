94e6ef14dfb9d7d0db0de2e79959aeea50fc6308
pedro-crs <crs.pedroh@gmail.com>
2024-12-06 16:53:11 -0300

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -7,19 +7,10 @@ router.post("/register", register);
 router.post("/login", login);
 
 router.get("/validateToken", verifyToken, (req, res) => {
-	const authHeader = req.headers.authorization;
-
-	if (!authHeader)
-		return res.status(401).json({ message: "Token não fornecido" });
-
-	const token = authHeader.split(" ")[1];
-
-	try {
-		const decoded = jwt.verify(token, process.env.JWT_SECRET);
-		res.json({ userId: decoded.id, userName: decoded.name });
-	} catch (error) {
-		return res.status(403).json({ message: "Token inválido ou expirado" });
-	}
+	res.json({
+		userId: req.user.id,
+		userName: req.user.name
+	});
 });
 
 module.exports = router;
